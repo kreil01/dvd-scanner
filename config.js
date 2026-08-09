ed3c99b516d04ddb49581c333559fd12bd19f8ae
kreil01 <118555862+kreil01@users.noreply.github.com>
2026-08-10 01:53:33 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.DVD_LOOKUP_WORKER_URL = "https://dvd-lookup-proxy.karen-kreilinger.workers.dev";
-window.SUPABASE_URL = "https://puxmogfkhhbprigopfmw.supabase.co/rest/v1/";
+window.SUPABASE_URL = "https://puxmogfkhhbprigopfmw.supabase.co";
 window.SUPABASE_ANON_KEY = "sb_publishable_DSovyYNW3DfbrvjW-QInbQ_AiWVWt3K";
